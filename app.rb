3172ebdb4aa3f06e773510e06d36b1f0ce1f7bcd
Jonathan Cutrell <jonathan@whiteboard.is>
2017-08-14 16:37:17 -0400

    updating for heroku

--- app.rb
+++ app.rb
@@ -1,29 +1,30 @@
 require 'nokogiri'
 require 'sinatra'
 require 'open-uri'
-require 'yaml'
 
-helpers do
-  def build_url(waypoint)
-    "https://nfdc.faa.gov/nfdcApps/services/airspaceFixes/fix_search.jsp?keyword=#{waypoint}"
-  end
-end
+class LookupFixApp < Sinatra::Base
 
-get '/' do
-  erb :index
-end
-
-post '/search' do
-  @waypoints = {} 
-  params[:waypoints].split(",").map do |waypoint|
-    doc = Nokogiri::HTML(open(build_url(waypoint.strip)))
-    @waypoints[waypoint] = doc.css('table tr').map do |row|
-      row.xpath('td').map do |cell|
-        "#{cell.text}"
-      end
+  helpers do
+    def build_url(waypoint)
+      "https://nfdc.faa.gov/nfdcApps/services/airspaceFixes/fix_search.jsp?keyword=#{waypoint}"
     end
   end
-  erb :search
+
+  get '/' do
+    erb :index
+  end
+
+  post '/search' do
+    @waypoints = {} 
+    params[:waypoints].split(",").map do |waypoint|
+      doc = Nokogiri::HTML(open(build_url(waypoint.strip)))
+      @waypoints[waypoint] = doc.css('table tr').map do |row|
+        row.xpath('td').map do |cell|
+          "#{cell.text}"
+        end
+      end
+    end
+    erb :search
+  end
+
 end
-
-
